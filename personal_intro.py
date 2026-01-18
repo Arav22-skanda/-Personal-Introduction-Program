cf15b73bb8ebb6dfdf99b43a1c46614679ea2041
Aravinth S. <aravinthsai22@gmail.com>
2026-01-18 13:07:13 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,27 @@
+print("Hello World")
+# Input your name
+name=input("What is your name ?")
+#Input field of interest
+field=input("What is your field of interest ?")
+#Display's your name & field of interest
+print(f"Welcome ,{name} with interest in {field}")
+# Input your age
+age=input("How old are you ?")
+# Display's your age
+print("I'm "+age)
+# Input your hobbies
+hobby=input("What are your hobbies ?")
+# Display's your hobbies
+print("Hobbies:"+ hobby)
+#Input the languages which you know
+lang=input("What are the languages Known by you ?")
+# Display's the languages known by you
+print("Languages:"+ lang)
+# Input the certifications done by you
+certificate=input("Have you done any certification courses ?")
+# Display's the certifications whic you know
+print("Certification courses:"+ certificate)
+# Input the skills which you know
+skill=input("Mention the skills which you have ?")
+# Display's the skills known by you
+print("Skills:"+ skill)
